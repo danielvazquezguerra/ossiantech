e8273489767b30f6a46f4aee80c865029ff546e0
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-08-03 19:56:16 +0200

    AddForm Structured

--- ot-pruebatec-front/src/app/app.module.ts
+++ ot-pruebatec-front/src/app/app.module.ts
@@ -5,18 +5,48 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+//Angular Material Components
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+
+//AntDesign Components 
+
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { es_ES } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+import { AddformComponent } from './components/addform/addform.component';
+
+registerLocaleData(es);
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    AddformComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgZorroAntdModule,
+    NzCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+  
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: es_ES, }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ot-pruebatec-front/src/app/components/main/main.component.ts
+++ ot-pruebatec-front/src/app/components/main/main.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
